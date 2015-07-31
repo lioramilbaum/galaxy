2e46b78cb774454f1bb1572dc6b45c370b691edc
liora <liora@lmb.co.il>
2015-07-31 20:20:44 +0300

    verbose

--- scripts/install_berks.sh
+++ scripts/install_berks.sh
@@ -4,6 +4,6 @@ sudo apt-get -y install python-software-properties
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install -y ruby2.2 ruby2.2-dev
-sudo gem install -y berkshelf --no-ri --no-rdoc
+sudo gem install -y berkshelf --no-ri --no-rdoc --verbose
 
 exit 0

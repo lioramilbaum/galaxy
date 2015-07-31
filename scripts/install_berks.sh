a52de28eb03de7d6361725938e8949cb82c52729
liora <liora@lmb.co.il>
2015-07-31 15:51:39 +0300

    types

--- scripts/install_berks.sh
+++ scripts/install_berks.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install ruby
-sudo apt-get install libxslt-dev libxml2-dev
+sudo apt-get install -y ruby libxslt-dev libxml2-dev
 gem install berkshelf --no-ri --no-rdoc
 
 exit 0

7197b7e75fe1d5afff18306a2b370384cd3395d8
liora <liora@lmb.co.il>
2015-07-31 21:45:18 +0300

    berks installation

--- scripts/install_berks.sh
+++ scripts/install_berks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get -y install python-software-properties g++
+sudo apt-get -y install python-software-properties g++ make
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install -y ruby2.2 ruby2.2-dev

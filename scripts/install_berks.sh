7a776861866313439608addfcdec579d9e30773f
liora <liora@lmb.co.il>
2015-08-01 21:14:57 +0300

    fix script errors

--- scripts/install_berks.sh
+++ scripts/install_berks.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install python-software-properties
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo apt-get install -y ruby2.2 ruby2.2-dev g++ make
+sudo apt-get install -y ruby2.2 ruby2.2-dev g++ make autoconf
 sudo gem install berkshelf --no-ri --no-rdoc --verbose
 
 exit 0

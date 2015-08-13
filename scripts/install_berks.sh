2cb8e891ad4608de00fcfe77e224048c1a8eee69
liora <liora@lmb.co.il>
2015-08-13 19:58:37 +0300

    Still not working

--- scripts/install_berks.sh
+++ scripts/install_berks.sh
@@ -4,6 +4,8 @@ sudo apt-get -y install python-software-properties
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install -y ruby2.2 ruby2.2-dev g++ make autoconf
-sudo gem install berkshelf --no-ri --no-rdoc --verbose
+sudo gem install bundler
+sudo bundle install
+#sudo gem install berkshelf --no-ri --no-rdoc --verbose
 
 exit 0

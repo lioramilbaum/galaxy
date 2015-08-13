a33a66800281d9afd5e598658ecf185ef3fceaca
liora <liora@lmb.co.il>
2015-08-13 21:55:38 +0300

    typo

--- scripts/install_berks.sh
+++ scripts/install_berks.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install python-software-properties
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install -y ruby2.2 ruby2.2-dev g++ make autoconf
-sudo gem install bundler
+#sudo gem install bundler
 #sudo bundle install --gemfile /packer/Gemfile
 #sudo gem install berkshelf --no-ri --no-rdoc --verbose
 

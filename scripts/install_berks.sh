47be0e8f64368ef52b40356b66a95cbf35d47a74
liora <liora@lmb.co.il>
2015-08-21 15:45:43 +0300

    CLM packer

--- scripts/install_berks.sh
+++ scripts/install_berks.sh
@@ -4,8 +4,8 @@ sudo apt-get -y install python-software-properties
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install -y ruby2.2 ruby2.2-dev g++ make autoconf
-#sudo gem install bundler
-#sudo bundle install --gemfile /packer/Gemfile
+sudo gem install bundler
+sudo bundle install --gemfile /packer/Gemfile
 #sudo gem install berkshelf --no-ri --no-rdoc --verbose
 
 exit 0

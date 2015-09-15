7c59d9d6df4b8757f1fbc926ef7fca4fa9e972d2
liora <liora@lmb.co.il>
2015-09-15 12:33:19 +0300

    Tasktop Sync

---
+++ cookbooks/SYNC/metadata.rb
@@ -0,0 +1,7 @@
+name             'SYNC'
+maintainer       'Liora Milbaum'
+maintainer_email 'liora@lmb.co.il'
+license          'MIT'
+description      'Tasktop Sync'
+long_description 'Tasktop Sync'
+version          '0.0.1'
